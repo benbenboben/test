b12a0dfb5e95e225636ecc48c68058d07dc1da43
bhellis725 <ellis.bh89@gmail.com>
2018-01-12 11:58:27 -0800

    Updated notebook.
    New plot - big stuff.

--- Untitled.py
+++ Untitled.py
@@ -4,7 +4,7 @@
 # # Table of Contents
 #  <p>
 
-# In[6]:
+# In[2]:
 
 
 import numpy as np
@@ -13,21 +13,41 @@ import matplotlib
 get_ipython().magic('matplotlib inline')
 
 
-# In[13]:
+# In[3]:
 
 
 x_rand = np.random.rand(100)
 
 
-# In[14]:
+# In[4]:
 
 
 y_rand = np.random.rand(100)
 
 
-# In[15]:
+# In[5]:
 
 
 fig, ax = plt.subplots()
 ax.scatter(x_rand, y_rand)
+
+
+# In[6]:
+
+
+x_rand = np.random.rand(500)
+y_rand = np.random.rand(500)
+
+
+# In[9]:
+
+
+fig, ax = plt.subplots()
+ax.hist2d(x_rand, y_rand);
+
+
+# In[ ]:
+
+
+
 
