384aee48cbf8fe7380760daf99cfa4173b53c236
bhellis725 <ellis.bh89@gmail.com>
2018-01-12 13:42:45 -0800

    test

--- Untitled.py
+++ Untitled.py
@@ -4,7 +4,7 @@
 # # Table of Contents
 #  <p>
 
-# In[2]:
+# In[1]:
 
 
 import numpy as np
@@ -13,33 +13,28 @@ import matplotlib
 get_ipython().magic('matplotlib inline')
 
 
-# In[3]:
+# In[2]:
 
 
 x_rand = np.random.rand(100)
-
-
-# In[4]:
-
-
 y_rand = np.random.rand(100)
 
 
-# In[5]:
+# In[3]:
 
 
 fig, ax = plt.subplots()
 ax.scatter(x_rand, y_rand)
 
 
-# In[6]:
+# In[4]:
 
 
 x_rand = np.random.rand(500)
 y_rand = np.random.rand(500)
 
 
-# In[9]:
+# In[5]:
 
 
 fig, ax = plt.subplots()
